408ed784992e188b14ecfa9fef3c2588c0e877bb
unknown <75951157+mohammed430@users.noreply.github.com>
2021-12-30 21:05:07 -0700

    angular startup

---
+++ portfolio/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BioComponent } from './bio/bio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
